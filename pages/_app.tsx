f69209c1e5771616c6d930623677a48d69254302
이자훈 <jh.lee@break.co.kr>
2021-10-07 16:15:12 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,15 +14,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           mutations: {
             retry: (failureCount, error: any) => {
               if (error?.message?.includes('401')) {
-                const isExistRefreshToken = !!localStorage.getItem('refreshToken');
-                if (isExistRefreshToken) {
-                  // refresh token 이 있다면 access token을 발급한 후 재시도한다
-                  localStorage.setItem('accessToken', 'true');
-                  return true;
-                } else {
-                  // refresh token 이 없다면 로그인페이지로 이동 (재시도 하지 않는다)
-                  return false;
-                }
+                localStorage.setItem('accessToken', 'true');
+                localStorage.setItem('refreshToken', 'true');
+                return true;
               }
             },
           },
